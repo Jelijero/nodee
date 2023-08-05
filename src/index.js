f7562903e1581032b6326db0ec12c85888228674
jelijero <bhavyablink2004@gmail.com>
2023-08-05 10:55:23 +0530

    2

--- src/index.js
+++ src/index.js
@@ -4,16 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function functioncomponent(){
+class StateManagement extends React.Component{
+  constructor(){
+    super()
+    this.state={year:"3rd"}
+  }
+
+first = () => this.setState({year:"1st"})
+second = () => this.setState({year:"2nd"})
+three = () => this.setState({year:"3rd"})
+four = () => this.setState({year:"4th"})
+render(){
   return (
     <div>
-      <h1>
-        this is bhavi
-      </h1>
-      <my componrnt/>
+      <h1>I am in {this.state.year} year </h1>
+      <button type="button" onClick={this.first}>1st</button>
+      <button type="button" onClick={this.second}>2nd</button>
+      <button type="button" onClick={this.three}>3rd</button>
+      <button type="button" onClick={this.four}>4th</button>
     </div>
-  );
+  )
 }
+}
+
+const roo = ReactDOM.createRoot(document.getElementById('roo'));
+roo.render(
+  <React.StrictMode>
+    <StateManagement />
+    <App />
+  </React.StrictMode>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
